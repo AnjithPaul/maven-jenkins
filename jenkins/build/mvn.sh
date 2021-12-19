6d11f87e75be325a560ac4d2e3883202ab47376a
AnjithPaul <anjithpaul.mec@gmail.com>
2021-12-19 20:59:05 +0530

    UPDATE. workspace in mvn scripts

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,6 @@ echo "******************"
 echo "***Building Jar***"
 echo "******************"
 
-docker run --rm -v $PWD/java-app:/app -v /Users/anjithpaul/.m2/:/root/.m2/ -w /app maven:3.5.4-alpine "$@"
+WORKSPACE=/Users/anjithpaul/Documents/thoughtworks/Learning/jenkins/jenkins-data/jenkins_home/workspace/pipeline-maven-jenkins
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /Users/anjithpaul/.m2/:/root/.m2/ -w /app maven:3.5.4-alpine "$@"
